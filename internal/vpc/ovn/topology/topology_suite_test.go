0471fd38eb4623db076558b981818b72d20dcace
Tarek Abu-Hariri <tabuhariri@nvidia.com>
2025-09-15 09:27:38 -0700

    fix: use ovn tag v24.03.0 in unit tests
    * fix: use ovn tag v24.03.0 in unit tests
    
    unit tests should use OVN v24.03.0 tag.
    
    Signed-off-by: Tarek Abu-Hariri <tabuhariri@nvidia.com>
    
    Based on release-v26.4@838526f86842081b9087d016fbf57bbc96eb3a3b

--- internal/vpc/ovn/topology/topology_suite_test.go
+++ internal/vpc/ovn/topology/topology_suite_test.go
@@ -30,7 +30,7 @@ import (
 )
 
 type Config struct {
-	ImageName string `envconfig:"OVN_CENTRAL_IMAGE_NAME" default:"ovn-central:v24.09.0"`
+	ImageName string `envconfig:"OVN_CENTRAL_IMAGE_NAME" default:"ovn-central:v24.03.0"`
 }
 
 var (
