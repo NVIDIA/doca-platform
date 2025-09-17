4251fae5f55571d52f9f0c75bb9a703a73f128e3
Adrian Chiris <adrianc@nvidia.com>
2025-09-17 00:36:46 -0700

    chore: move vfmac out of vpc tree
    * chore: move vfmac out of vpc tree
    
    - move vfmac binary under dpf root/cmd dir and
      rename it to dpuagent
    - move vfmac package under pkg as its
      being used by both vpc and dp-agent
    - remove vfmac init container from vpc chart
      as vfmac is now delivered part of bfb and invoked
      as system service
    - add build target for dpuagent binary
    - run go mod tidy to update dependencies
    
    Signed-off-by: adrianc <adrianc@nvidia.com>
    
    Based on release-v25.10@dff45965e54465d03563ad6adb8d32b911572711

---
+++ cmd/dpuagent/main.go
@@ -0,0 +1,38 @@
+/*
+Copyright 2025 NVIDIA
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"log"
+
+	"github.com/nvidia/doca-platform/pkg/utils/networkhelper"
+	"github.com/nvidia/doca-platform/pkg/vfmac"
+)
+
+func main() {
+	// Create a new VFMAC instance with default configuration
+	vfmacInstance, err := vfmac.NewVFMAC(nil, networkhelper.New(), "", "")
+	if err != nil {
+		log.Fatalf("[ERROR] Error creating VFMAC instance: %v", err)
+	}
+
+	// Process VFs using the new instance
+	if err := vfmacInstance.ProcessVFs(); err != nil {
+		log.Fatalf("[ERROR] Error processing VFs: %v", err)
+	}
+	log.Printf("[INFO] Successfully processed VF MAC addresses")
+}
