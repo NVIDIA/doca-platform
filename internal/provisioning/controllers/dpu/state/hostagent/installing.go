55ad9ed23730f96c6976294bcac641afa76571ab
Peng Wang (Networking SW) <pengwang@nvidia.com>
2025-10-20 19:21:26 -0700

    chore: move label update from os install to dpu creation
    * chore: move label update from os install to dpu creation
    
    chore: move label update from os install to dpu creation
    Signed-off-by: Peng Wang <pengwang@nvidia.com>
    
    Based on release-v26.8@891fc03d61ecb7d516c597f55e53a9f75abe6baa

--- internal/provisioning/controllers/dpu/state/hostagent/installing.go
+++ internal/provisioning/controllers/dpu/state/hostagent/installing.go
@@ -18,17 +18,14 @@ package hostagent
 
 import (
 	"context"
-	"fmt"
 
 	provisioningv1 "github.com/nvidia/doca-platform/api/provisioning/v1alpha1"
 	dutil "github.com/nvidia/doca-platform/internal/provisioning/controllers/dpu/util"
 	cutil "github.com/nvidia/doca-platform/internal/provisioning/controllers/util"
 	"github.com/nvidia/doca-platform/internal/provisioning/hostagent/phase/install"
-	"github.com/nvidia/doca-platform/internal/release"
 
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
@@ -59,25 +56,6 @@ func Installing(ctx context.Context, dpu *provisioningv1.DPU, ctrlCtx *dutil.Con
 			logger.Info("Installation in progress", "status", condition.Status, "reason", condition.Reason)
 		}
 		return *state, nil
-	}
-
-	if dpu.Spec.Cluster.NodeLabels == nil {
-		dpu.Spec.Cluster.NodeLabels = make(map[string]string)
-	}
-
-	// Add DOCA_BFB version to DPU NodeLabels as it is found in DPU ARM under /etc/mlnx-release. e.g.,
-	// cat /etc/mlnx-release
-	// bf-bundle-2.7.0-33_24.04_ubuntu-22.04_prod
-	patch := client.MergeFrom(dpu.DeepCopy())
-	dpu.Spec.Cluster.NodeLabels[cutil.HostNameDPULabelKey] = dpu.Spec.DPUNodeName
-	// Set the DPU version in the node labels. This is necessary to be able to schedule Pods
-	// on the DPU Node based on the DPF version.
-	// The DPF version in the status should never be nil, but we check it here to avoid nil pointer dereference.
-	if dpu.Status.DPFVersion != nil {
-		dpu.Spec.Cluster.NodeLabels[release.DPFVersionLabelKey] = *dpu.Status.DPFVersion
-	}
-	if err := ctrlCtx.Client.Patch(ctx, dpu, patch); err != nil {
-		return *state, fmt.Errorf("failed to patch DPU: %w", err)
 	}
 
 	state.Phase = provisioningv1.DPUCheckingHostRebootNeed
