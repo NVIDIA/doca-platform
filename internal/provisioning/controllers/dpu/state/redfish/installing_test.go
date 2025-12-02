de83a3daf68968c2cca13b3a018de25ad39d2b73
Ian Lang <ilang@nvidia.com>
2025-12-02 00:19:04 -0800

    fix: trim scheme from redfish bfb image URL
    * docs: replace deprecated ZT field
    
    replace deprecated ZT field
    
    Signed-off-by: Ian Lang <ilang@nvidia.com>
    
    * fix: trim scheme from redfish bfb image URL
    
    trim scheme from redfish bfb image URL
    
    Signed-off-by: Ian Lang <ilang@nvidia.com>
    
    Based on release-v26.4@abdf9e81f222e2ab845a9f99d6e1ba47bbc87754

--- internal/provisioning/controllers/dpu/state/redfish/installing_test.go
+++ internal/provisioning/controllers/dpu/state/redfish/installing_test.go
@@ -17,18 +17,21 @@ limitations under the License.
 package redfish
 
 import (
-	"testing"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
-func TestConcatBFBAndBFCFGPath(t *testing.T) {
-	registry := "10.0.110.1:8080"
-	bfbFile := "/bfb/dpf-operator-system-bfb-bundle.bfb"
-	bfcfgFile := "/bfb/bfcfg/dpf-operator-system_dpu-node-mt25066004be-mt25066004be_ea091a0e-f0ae-4033-9db3-2ecf9a1dfe61"
-	expected := "10.0.110.1:8080/bfb/??dpf-operator-system-bfb-bundle.bfb,bfcfg/dpf-operator-system_dpu-node-mt25066004be-mt25066004be_ea091a0e-f0ae-4033-9db3-2ecf9a1dfe61?/bfb-to-install"
+var _ = Describe("Installing", func() {
+	Context("concatBFBAndBFCFGPath", func() {
+		It("should return the correct path", func() {
+			registry := "10.0.110.1:8080"
+			bfbFile := "/bfb/dpf-operator-system-bfb-bundle.bfb"
+			bfcfgFile := "/bfb/bfcfg/dpf-operator-system_dpu-node-mt25066004be-mt25066004be_ea091a0e-f0ae-4033-9db3-2ecf9a1dfe61"
+			expected := "10.0.110.1:8080/bfb/??dpf-operator-system-bfb-bundle.bfb,bfcfg/dpf-operator-system_dpu-node-mt25066004be-mt25066004be_ea091a0e-f0ae-4033-9db3-2ecf9a1dfe61?/bfb-to-install"
 
-	got := concatBFBAndBFCFGPath(registry, bfbFile, bfcfgFile)
-	if got != expected {
-		t.Errorf("concatBFBAndBFCFGPath(%s, %s) = %s, want %s", bfbFile, bfcfgFile, got, expected)
-	}
-
-}
+			Expect(concatBFBAndBFCFGPath(registry, bfbFile, bfcfgFile)).To(Equal(expected))
+			Expect(concatBFBAndBFCFGPath("http://"+registry, bfbFile, bfcfgFile)).To(Equal(expected))
+			Expect(concatBFBAndBFCFGPath("https://"+registry, bfbFile, bfcfgFile)).To(Equal(expected))
+		})
+	})
+})
