55e93c76f8cbe2f918d76dfff488e7a4c1e51bdc
William Wang <willwang@nvidia.com>
2025-12-18 00:24:52 -0800

    chore: remove HostPowerCycleRequired from DPUFlavorNVConfig
    * chore: remove HostPowerCycleRequired from DPUFlavorNVConfig
    
    This PR is to deprecate the HostPowerCycleRequired
    
    Signed-off-by: willwang <willwang@nvidia.com>
    
    Based on release-v26.4@c3f2b65064313c609e632753543c9baa8f66bbd3

--- api/provisioning/v1alpha1/dpuflavor_types.go
+++ api/provisioning/v1alpha1/dpuflavor_types.go
@@ -95,11 +95,6 @@ type NVConfig struct {
 	// Parameters are the parameters to be set for the device.
 	// +optional
 	Parameters []string `json:"parameters,omitempty"`
-	// HostPowerCycleRequired indicates if the host needs to be power cycled after applying the configuration.
-	//
-	// Deprecated: This field is unused and deprecated. It will be removed with the release v26.4.0.
-	// +optional
-	HostPowerCycleRequired *bool `json:"hostPowerCycleRequired,omitempty"`
 }
 
 type DPUFlavorOVS struct {
