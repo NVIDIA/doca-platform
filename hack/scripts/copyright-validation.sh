3880ad64d61104c2fd62d85ece78db0d7dafd7b9
Noam Hofshi <nhofshi@nvidia.com>
2026-01-13 04:03:54 -0800

    chore: align all copyright to camel case
    * fix: change copyright validation as well
    
    Change the copyright validation code as well
    
    Signed-off-by: Noam Hofshi <nhofshi@nvidia.com>
    
    * chore: align all copyright to camel case
    
    Align all COPYRIGHTS and boilerplate files to Copyright
    
    Signed-off-by: Noam Hofshi <nhofshi@nvidia.com>
    
    Based on main@cffb635faf6232b6d46594b5a875b83331302522

--- hack/scripts/copyright-validation.sh
+++ hack/scripts/copyright-validation.sh
@@ -43,7 +43,7 @@ print_copyrights() {
 	echo "Please use the following copy right notice in the beginning of the failed files:"
 	echo "----------------------------------------------------"
 	echo "
-  COPYRIGHT $(date +%Y) NVIDIA
+  Copyright $(date +%Y) NVIDIA
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
