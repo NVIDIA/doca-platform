69eb095e0c4ffeb31e75eb263c8deed19d31f5d0
Alin Serdean <aserdean@nvidia.com>
2026-01-22 10:05:28 -0800

    chore: add OVN-Kubernetes only E2E tests
    * fix: change permissions for script ci-provision-ovnk-cloud.sh
    
    This patch changes permissions for script ci-provision-ovnk-cloud.sh
    
    Signed-off-by: Alin Serdean <aserdean@nvidia.com>
    
    * fix: reduce e2e jobs titles
    
    this commit reduces e2e job titles
    
    Signed-off-by: Alin Serdean <aserdean@nvidia.com>
    
    * chore: add OVN-Kubernetes only E2E tests
    
    This patch adds OVN-Kubernetes E2E tests
    Signed-off-by: Alin Gabriel Serdean aserdean@nvidia.com
    Signed-off-by: Alin Serdean <aserdean@nvidia.com>
    
    Based on main@f4bc93e7306b5942c2131bdeb37cd1a825ef467c

--- test/e2e/ovnkhbn.go
+++ test/e2e/ovnkhbn.go
@@ -21,83 +21,8 @@ import (
 	"time"
 
 	"github.com/nvidia/doca-platform/test/utils/dpuservice"
-	"github.com/nvidia/doca-platform/test/utils/netshoot"
-
-	. "github.com/onsi/ginkgo/v2"
 )
-
-func VerifyPerformancePodToPodSameNode(ctx context.Context, input *systemTestInput) {
-	if !input.hasDpuNodes() {
-		Skip("Skip test as there are not multiple nodes")
-	}
-
-	hostNamespace := "ovnkhbn-same-node"
-	createTestNamespace(ctx, input.client, hostNamespace)
-
-	By("creating test pods")
-	pod1Config, pod2Config := getPodSameNodeConfigs(ctx, input, hostNamespace)
-	netshoot.CreateAndWaitForPods(ctx, input.client, []*netshoot.TestPodConfig{&pod1Config, &pod2Config})
-
-	By("get pod2 IP")
-	pod2IP := netshoot.GetPodIP(ctx, input.client, hostNamespace, pod2Config.Name)
-
-	By("running traffic test between pods")
-	netshoot.RunTrafficTest(&hostClusterRESTClient, &input.restConfig, hostNamespace, pod1Config.Name, pod2Config.Name, pod2IP)
-}
-
-func VerifyPerformancePodToPodDifferentNode(ctx context.Context, input *systemTestInput) {
-	if !input.hasDpuNodes() {
-		Skip("Skip test as there are not multiple nodes")
-	}
-
-	hostNamespace := "ovnkhbn-different-node"
-	createTestNamespace(ctx, input.client, hostNamespace)
-
-	By("creating test pods")
-	pod1Config, pod2Config := getPodDifferentNodeConfigs(ctx, input, hostNamespace)
-	netshoot.CreateAndWaitForPods(ctx, input.client, []*netshoot.TestPodConfig{&pod1Config, &pod2Config})
-
-	By("get pod2 IP")
-	pod2IP := netshoot.GetPodIP(ctx, input.client, hostNamespace, pod2Config.Name)
-
-	By("running traffic test between pods")
-	netshoot.RunTrafficTest(&hostClusterRESTClient, &input.restConfig, hostNamespace, pod1Config.Name, pod2Config.Name, pod2IP)
-}
 
 func WaitForOVNKHBNDeploymentReady(ctx context.Context, input *systemTestInput) {
 	dpuservice.WaitForDPUDeploymentReady(ctx, input.client, dpfOperatorSystemNamespace, []string{"ovn-hbn"}, 50*time.Minute)
-}
-
-func getPodDifferentNodeConfigs(ctx context.Context, input *systemTestInput, namespace string) (netshoot.TestPodConfig, netshoot.TestPodConfig) {
-	workerNode1, workerNode2 := getTwoWorkerNodeNames(ctx, input.client)
-
-	pod1Config := netshoot.TestPodConfig{
-		Name:      "pod1",
-		Namespace: namespace,
-		NodeName:  workerNode1,
-	}
-	pod2Config := netshoot.TestPodConfig{
-		Name:      "pod2",
-		Namespace: namespace,
-		NodeName:  workerNode2,
-	}
-
-	return pod1Config, pod2Config
-}
-
-func getPodSameNodeConfigs(ctx context.Context, input *systemTestInput, namespace string) (netshoot.TestPodConfig, netshoot.TestPodConfig) {
-	workerNode1, _ := getTwoWorkerNodeNames(ctx, input.client)
-
-	pod1Config := netshoot.TestPodConfig{
-		Name:      "pod1",
-		Namespace: namespace,
-		NodeName:  workerNode1,
-	}
-	pod2Config := netshoot.TestPodConfig{
-		Name:      "pod2",
-		Namespace: namespace,
-		NodeName:  workerNode1,
-	}
-
-	return pod1Config, pod2Config
 }
