0e158933fe0d6af412b90b3ae335faab536b7702
Tobias Giese <tgiese@nvidia.com>
2026-04-29 00:24:01 -0700

    fix(api): rename Monitoring.Disabled to Disable
    * fix(api): rename Monitoring.Disabled to Disable
    
    Align the MonitoringConfiguration field name with the convention
    used by other component configs (e.g. BaseComponentConfig.Disable).
    Update deepcopy, controller tests, and e2e tests accordingly.
    
    Signed-off-by: Tobias Giese <tgiese@nvidia.com>
    
    Based on main@e07d7fac59e17b3c39f19b07790480522543e55a

--- test/e2e/system_test.go
+++ test/e2e/system_test.go
@@ -75,7 +75,7 @@ func SetInput() {
 				},
 			},
 			Monitoring: &operatorv1.MonitoringConfiguration{
-				Disabled: ptr.To(false),
+				Disable: ptr.To(false),
 				OpenTelemetryCollector: &operatorv1.OpenTelemetryCollectorConfiguration{
 					Logging: &operatorv1.OpenTelemetryCollectorLoggingConfiguration{
 						Endpoint: fmt.Sprintf("%s%s:%d", otelEndpointSchema, controlPlaneIP, otelNodePort),
