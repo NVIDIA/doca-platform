0ebbb50d8f1f1464ba3b55a744dcfb6c08c0e105
Peng Wang (Networking SW) <pengwang@nvidia.com>
2026-05-14 23:26:30 -0700

    fix: upgrade failed due to dpuflavor immutable issue
    * fix: upgrade failed due to dpuflavor immutable issue
    
    * fix: upgrade failed due to dpuflavor immutable issue
    
    fix: upgrade failed due to dpuflavor immutable issue
    Signed-off-by: Peng Wang <pengwang@nvidia.com>
    
    (cherry picked from commit 1139d52291c374b4cefb0a8882b876d3bdf6d9e0)
    
    7a2c6e97 fix: upgrade failed due to dpuflavor immutable issue
    
    Co-authored-by: Peng Wang (Networking SW) <pengwang@nvidia.com>
    
    Based on release-v26.4@ae560e62bb2e335c0a77522b61c37401ac2a920f

--- internal/provisioning/webhooks/dpuflavor_webhook.go
+++ internal/provisioning/webhooks/dpuflavor_webhook.go
@@ -33,7 +33,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
-// +kubebuilder:webhook:path=/mutate-provisioning-dpu-nvidia-com-v1alpha1-dpuflavor,mutating=true,failurePolicy=fail,sideEffects=None,groups=provisioning.dpu.nvidia.com,resources=dpuflavors,verbs=create;update,versions=v1alpha1,name=mdpuflavor.kb.io,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/mutate-provisioning-dpu-nvidia-com-v1alpha1-dpuflavor,mutating=true,failurePolicy=fail,sideEffects=None,groups=provisioning.dpu.nvidia.com,resources=dpuflavors,verbs=create,versions=v1alpha1,name=mdpuflavor.kb.io,admissionReviewVersions=v1
 // +kubebuilder:webhook:path=/validate-provisioning-dpu-nvidia-com-v1alpha1-dpuflavor,mutating=false,failurePolicy=fail,sideEffects=None,groups=provisioning.dpu.nvidia.com,resources=dpuflavors,verbs=create;update;delete,versions=v1alpha1,name=vdpuflavor.kb.io,admissionReviewVersions=v1
 
 // DPUFlavor implements a webhook for the DPUFlavor object.
